7519469cafb91bc6ba65528a5ce1990b014a45e5
Alex <black.s.w.a.n@yandex.ru>
2023-03-11 15:13:10 +0300

    NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,52 @@
 export default class NotificationMessage {
+  subElement = {};
+  constructor(title = '', {duration = 1000, type = 'success'} = {}) {
+    this.title = title;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+    this.getButton();
+  }
 
+  template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.title}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template();
+    this.element = wrapper.firstElementChild;
+  }
+
+  show(target = document.body) {
+    const notificationEl = document.querySelector('.notification');
+    if (notificationEl) {
+      notificationEl.remove();
+      clearTimeout(window.timerId);
+    }
+    target.append(this.element);
+    window.timerId = setTimeout(() => this.remove(), this.duration);
+  }
+
+  getButton() {
+    this.subElement.button = document.querySelector('#btn1');
+  }
+
+  remove = () => {
+    this.element.remove();
+  }
+
+  destroy = () => {
+    this.remove();
+  }
 }
